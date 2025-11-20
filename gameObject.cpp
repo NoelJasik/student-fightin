269164b25945ae22c14e77850983a5e73ad6dae2
NoelJasik <Noeloskar99@proton.me>
2025-11-20 15:40:37 +0100

    podmianka (zrobiłem klase w headerze xd)

--- gameObject.cpp
+++ gameObject.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "gameObject.h"
+#include "SDL.h"
+#include <string>
+
+class gameObject {
+public:
+    SDL_Rect rect;
+    std::string name;
+    float hp;
+    gameObject(int _x, int _y, int _w, int _h, std::string _name, float _hp) {;
+        rect.x = _x;
+        rect.y = _y;
+        rect.w = _w;
+        rect.h = _h;
+        name = _name;
+        hp = _hp;
+    }
+
+
+};
--- gameObject.h
+++ gameObject.h
@@ -2,25 +2,7 @@
 
 #ifndef UNIPROJECT_GAMEOBJECT_H
 #define UNIPROJECT_GAMEOBJECT_H
-#include "SDL.h"
-#include <string>
 
-class gameObject {
-public:
-    SDL_Rect rect;
-    std::string name;
-    float hp;
-    gameObject(int _x, int _y, int _w, int _h, std::string _name, float _hp) {;
-        rect.x = _x;
-        rect.y = _y;
-        rect.w = _w;
-        rect.h = _h;
-        name = _name;
-        hp = _hp;
-    }
-
-
-};
 
 
 #endif //UNIPROJECT_GAMEOBJECT_H
