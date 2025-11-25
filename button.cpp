1b0fa137d573696731b951598db6aac3cd9ec5c1
Dawid <dawidros@Dawids-MacBook-Pro.local>
2025-11-25 20:23:30 +0100

    dodawanie przycisku powoli

---
+++ button.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dawid Roś on 25/11/2025.
+//
+
+#include "button.h"
---
+++ button.h
@@ -0,0 +1,18 @@
+//
+// Created by Dawid Roś on 25/11/2025.
+//
+
+#ifndef UNIPROJECT_BUTTON_H
+#define UNIPROJECT_BUTTON_H
+#include "SDL_rect.h"
+
+
+class button {
+    public:
+    SDL_texture *tex;
+    SDL_Rect rect; drect;
+
+};
+
+
+#endif //UNIPROJECT_BUTTON_H
